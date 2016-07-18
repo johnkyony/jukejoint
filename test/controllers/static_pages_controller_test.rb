6ce575dca80eb73d6cd4e7c67bef6ed13a010bec
jkyony <jkyony@gmail.com>
2016-07-18 20:37:59 +0100

    fix

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,19 +15,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Juke joint , Hand picked Music for your soul"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Juke joint , Hand picked Music for your soul"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Juke joint , Hand picked Music for your soul"
   end
 
 end
